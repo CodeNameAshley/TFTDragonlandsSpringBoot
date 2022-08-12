46be2bcf284e28ac1ad900f2c262088758cd6ebe
Ashley <ashley.navo@skybettingandgaming.com>
2022-08-12 11:26:23 +0100

    Re-worked Dragons file

--- src/main/java/com/TFTDragonlands/TFTDragonlands/TFTDragons/Dragons.java
+++ src/main/java/com/TFTDragonlands/TFTDragonlands/TFTDragons/Dragons.java
@@ -1,5 +1,6 @@
 package com.TFTDragonlands.TFTDragonlands.TFTDragons;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +11,14 @@ import javax.persistence.Table;
 @Entity
 @Table
 public class Dragons {
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
     @Id
     @SequenceGenerator(
             name = "tftdragonlands_sequence",
@@ -20,6 +29,8 @@ public class Dragons {
             strategy = GenerationType.SEQUENCE,
             generator = "tftdragonlands_sequence"
     )
+
+    private Long id;
     private String name;
     private String trait;
     private int cost;
@@ -29,7 +40,13 @@ public class Dragons {
     public Dragons() {
     }
 
-    public Dragons(String name, String trait, int cost, int health, int mana ) {
+    public Dragons(Long id,
+                   String name,
+                   String trait,
+                   int cost,
+                   int health,
+                   int mana ) {
+        this.id = id;
         this.name = name;
         this.cost = cost;
         this.health = health;
@@ -37,54 +54,85 @@ public class Dragons {
         this.trait = trait;
     }
 
+    public Dragons(String name,
+                   String trait,
+                   int cost,
+                   int health,
+                   int mana) {
+        this.name = name;
+        this.trait = trait;
+        this.cost = cost;
+        this.health = health;
+        this.mana = mana;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getCost() {
+
         return cost;
     }
 
     public void setCost(int cost) {
+
         this.cost = cost;
     }
 
     public int getHealth() {
+
         return health;
     }
 
     public void setHealth(int health) {
+
         this.health = health;
     }
 
     public int getMana() {
+
         return mana;
     }
 
     public void setMana(int mana) {
+
         this.mana = mana;
     }
 
     public String getTrait() {
+
         return trait;
     }
 
     public void setTrait(String trait) {
+
         this.trait = trait;
     }
 
     @Override
     public String toString() {
         return "Dragons{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", trait='" + trait + '\'' +
                 ", cost=" + cost +
                 ", health=" + health +
                 ", mana=" + mana +
-                ", trait='" + trait + '\'' +
                 '}';
     }
 }
